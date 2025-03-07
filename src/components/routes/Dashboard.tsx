b8487cac04c8f3b170c9224b87127f82b3003083
Nampally Rohith <rohithnampelly57@gmail.com>
2025-03-07 13:41:42 +0530

    uncommented profile component

--- src/components/routes/Dashboard.tsx
+++ src/components/routes/Dashboard.tsx
@@ -4,7 +4,7 @@ import Profile from "@/shared/Profile";
 const Dashboard = () => {
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center">
-      {/* <Profile /> */}
+      <Profile />
       <CalendarForm />
     </div>
   );
