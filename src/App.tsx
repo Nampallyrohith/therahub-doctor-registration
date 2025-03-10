5d3807d04d35f0b1c62a0e674327cd0f73f57117
Nampally Rohith <rohithnampelly57@gmail.com>
2025-03-10 12:38:43 +0530

    moved header component to dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import Authentication from "./components/routes/Authentication";
 import Dashboard from "./components/routes/Dashboard";
 import { Route, Routes } from "react-router-dom";
-import Header from "./shared/Header";
 
 function App() {
   return (
     <>
-      <Header />
       <Routes>
         <Route path="/authenication" element={<Authentication />} />
         <Route path="/dashboard" element={<Dashboard />} />
--- src/components/routes/Dashboard.tsx
+++ src/components/routes/Dashboard.tsx
@@ -1,12 +1,17 @@
 import CalendarForm from "@/shared/CalendarForm";
+import Header from "@/shared/Header";
 import Profile from "@/shared/Profile";
 
 const Dashboard = () => {
   return (
+    <>
+    <Header />
     <div className="w-full h-full flex flex-col my-10 items-center">
       <Profile />
       <CalendarForm />
     </div>
+    </>
+    
   );
 };
 
