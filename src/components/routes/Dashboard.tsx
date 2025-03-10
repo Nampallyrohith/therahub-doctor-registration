a3debb9e9f9391a29b1912f2a3f431c943604471
[Lokesh Pasam] <[valid-200303124405@paruluniversity.ac.in]>
2025-03-10 17:50:09 +0530

    add leave list container

--- src/components/routes/Dashboard.tsx
+++ src/components/routes/Dashboard.tsx
@@ -1,11 +1,13 @@
 import CalendarForm from "@/shared/CalendarForm";
 import Profile from "@/shared/Profile";
+import LeaveList from "@/shared/LeaveList";
 
 const Dashboard = () => {
   return (
     <div className="w-full h-full flex flex-col my-10 items-center">
       <Profile />
       <CalendarForm />
+      <LeaveList />
     </div>
   );
 };
