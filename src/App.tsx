9a33ec0299ba22e04c66c4e35a639d358da4a836
[Lokesh Pasam] <[valid-200303124405@paruluniversity.ac.in]>
2025-03-13 10:56:41 +0530

    added route logic by default

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import Authentication from "./components/routes/Authentication";
 import Dashboard from "./components/routes/Dashboard";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Navigate to="/authentication" replace />} />
         <Route path="/authentication" element={<Authentication />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
