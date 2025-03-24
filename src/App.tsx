efdc57ee7cd0974116b839137b1e11da53551112
Nampally Rohith <rohithnampelly57@gmail.com>
2025-03-24 11:42:57 +0530

    added new component and route chekcing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,43 @@
+import { JSX } from "react";
 import Authentication from "./components/routes/Authentication";
 import Dashboard from "./components/routes/Dashboard";
 import { Route, Routes, Navigate } from "react-router-dom";
+import NotFound from "./shared/NotFound";
+
+const PrivateRoute = ({ children }: { children: JSX.Element }) => {
+  const token = localStorage.getItem("doctorToken");
+  return token ? children : <Navigate to="/authentication" replace />;
+};
+
+const PublicRoute = ({ children }: { children: JSX.Element }) => {
+  const token = localStorage.getItem("doctorToken");
+  return token ? <Navigate to="/dashboard" replace /> : children;
+};
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Navigate to="/authentication" replace />} />
-        <Route path="/authentication" element={<Authentication />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Navigate to="/authentication" replace />} />
+
+      <Route
+        path="/authentication"
+        element={
+          <PublicRoute>
+            <Authentication />
+          </PublicRoute>
+        }
+      />
+
+      <Route
+        path="/dashboard"
+        element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        }
+      />
+      <Route path="/*" element={<NotFound />} />
+    </Routes>
   );
 }
 
